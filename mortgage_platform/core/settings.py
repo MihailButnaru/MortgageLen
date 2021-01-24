8fb4257341727dc793a93e115d099234a17e31bf
Mihail Butnaru <butnarum@hotmail.com>
2021-01-24 20:42:58 +0000

    add (rest_framework, mortgage app to settings)

--- mortgage_platform/core/settings.py
+++ mortgage_platform/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'mortgage'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {}
 }
 
 
